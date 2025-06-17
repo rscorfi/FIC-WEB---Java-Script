8d4664b09af669daa5aeb6fb88b0d23e70fa95fa
Rafael Scorfi Gatto <rscorfi@gmail.com>
2025-06-17 17:47:48 +0000

    Upd Ex10

--- Aula 08/Exercícios/A08E10.js
+++ Aula 08/Exercícios/A08E10.js
@@ -11,16 +11,3 @@ for (let i = 0; i < n; i++) {
     n1 = n2
     n2 = n3
 }
-
-// n = int(input("Informe a quantidade de termos de Fibonacci:"))
-// n1 = 0
-// n2 = 1
-// n3 = 0
-// i = 0
-// a = 0
-
-// for i in range (n):
-//     print (n1)
-//     n3 = n1 + n2
-//     n1 = n2
-//     n2 = n3
