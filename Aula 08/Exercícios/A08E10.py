54b4a84c83dabe3a7348f87e6e0b88b4e3d22cff
Rafael Scorfi Gatto <rscorfi@gmail.com>
2025-06-17 14:35:12 +0000

    upd E10 py

--- Aula 08/Exercícios/A08E10.py
+++ Aula 08/Exercícios/A08E10.py
@@ -1,12 +1,13 @@
-n = input("Informe a quantidade de termos de Fibonacci:")
-# let n = Number(prompt('Informe a quantidade de termos de Fibonacci:'))
+n = int(input("Informe a quantidade de termos de Fibonacci:"))
+n1 = 0
+n2 = 1
+n3 = 0
 i = 0
 nfib = 0
-# let n1 = 0
-# let n2 = 1
-# console.log(n1)
-# console.log(n2)
 
 for i in range (n):
-    nfib = (i-1) + (i-2)
-    print (nfib)
+    if i <= 1:
+        print (i)
+    else:
+        n3 = n1 + n2
+        n1
