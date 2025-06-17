c7c346aea5f09076c4ac6db0ea5e0bb3f5e92a39
Rafael Scorfi Gatto <rscorfi@gmail.com>
2025-06-16 21:55:22 -0300

    upd aula 08 ex 10 - não concluido

--- Aula 08/Exercícios/A08E10.js
+++ Aula 08/Exercícios/A08E10.js
@@ -1,5 +1,8 @@
 // 10. Crie um programa que gere a sequência de "Fibonacci" até o n-ésimo termo, onde n é informado pelo usuário. É uma sequência de números inteiros, começando por 0 e 1. Os números subsequentes corresponde a soma dos dois números anteriores. Ex: 0,1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89...
 
+
+// não está pronto
+
 let n = Number(prompt('Informe a quantidade de termos de Fibonacci:'))
 let n1 = 0
 let n2 = 1
