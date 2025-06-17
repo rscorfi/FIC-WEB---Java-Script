096c8097fa2bb73993f0a8c9913c1b56d05e929d
Rafael Scorfi Gatto <rscorfi@gmail.com>
2025-06-17 13:45:16 -0300

    upd A08E10 py

--- Aula 08/Exercícios/A08E10.py
+++ Aula 08/Exercícios/A08E10.py
@@ -3,11 +3,10 @@ n1 = 0
 n2 = 1
 n3 = 0
 i = 0
-nfib = 0
 
 for i in range (n):
-    if i <= 1:
-        print (i)
-    else:
-        n3 = n1 + n2
-        n1
+    print (n1)
+    print (n2)
+    n3 = n1 + n2
+    n1 = n2
+    n2 = n3
